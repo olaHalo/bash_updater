711683fd364cdc3d944b51d9d9c95fe6e35fecdf
olaHalo <30677521+olaHalo@users.noreply.github.com>
2017-08-28 09:09:04 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,4 +8,3 @@ apt upgrade
 
 echo "Updater script has run on " $mydate >> /var/log/updateScript.log
 
-curl -X POST --data-urlencode 'payload={"text": "COC Workstation Updater has *started*", "username": "Baby_aliens", "icon_emoji": ":aliens:"}' https://hooks.slack.com/services/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
